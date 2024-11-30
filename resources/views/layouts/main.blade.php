7159959cb3223d8c1a2f0f54f6a0037f0cb06f5b
Vitor <vitordevestudos@gmail.com>
2024-11-30 00:01:14 -0300

    Implementação da aula 08 - Arrumando o link com o BootStrap e o header

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,11 +12,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
 
         {{-- Css do Bootstrap --}}
-        <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-pzjw8f+ua7Kw1TIq8TV56Wj6JtZyI8yXKAwwcPwnVf0nk7qStn6N9Ydz1mG8TiF"
-        crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
         <script src="//unpkg.com/alpinejs" defer></script>
         {{-- Css da Aplicação --}}
@@ -53,10 +49,6 @@
             <p>HDC Events &copy; 2024</p>
         </footer>
 
-        <script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-HoA1dDD+7wb2hS58sAz9c/gtAvDAGAIPlJ+3c6aATKGtoChkjZx1HgEacFuVOoPz"
-        crossorigin="anonymous">
-        </script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </body>
 </html>
